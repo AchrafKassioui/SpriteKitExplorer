841d0fc2bb971098843e5eebb33933784d4a61b3
Achraf Kassioui <achrafkassioui@gmail.com>
2024-01-19 04:48:06 +0100

    Initial Commit

---
+++ SpriteKitExplorer/SpriteKitExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitExplorerApp.swift
+//  SpriteKitExplorer
+//
+//  Created by Achraf Kassioui on 19/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
